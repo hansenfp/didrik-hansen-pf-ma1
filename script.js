956fe872fa0c750b3178beae761d4f128d8afcc5
hansenfp <61291503+hansenfp@users.noreply.github.com>
2020-09-27 20:52:09 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+// 1
+var firstVar
+
+//2
+var name = Didrik;
+
+//3
+var number = 1;
+
+//4
+var division = 20 / 5;
+
+//5
+var frog = animal
+
+typeof frog
+
+//6
+var orderHasShipped = true;
+
+//7
+if (orderHasShipped) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+//8
+for (var i = 0; i < 9; i++) {
+    console.log(i);
+}
